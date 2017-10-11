773c218b0197a7439e23f1c3239770feff786d52
Brian C Sparks <brian.c.sparks@hp.com>
2017-10-11 21:52:29 +0000

    Added permission to buckets and keys

--- s3/routes/s3-get.js
+++ s3/routes/s3-get.js
@@ -13,6 +13,7 @@ const setOnn                  = sg.setOnn;
 const deref                   = sg.deref;
 const argvGet                 = sg.argvGet;
 const argvExtract             = sg.argvExtract;
+const safeNewRegExp           = sg.safeNewRegExp            || mySafeNewRegExp;
 const myColor                 = serverassist.myColor();
 const myStack                 = serverassist.myStack();
 const registerAsService       = serverassist.registerAsService;
@@ -20,19 +21,20 @@ const registerAsServiceApp    = serverassist.registerAsServiceApp;
 const configuration           = serverassist.configuration;
 const _200                    = serverassist._200;
 const _400                    = serverassist._400;
+const _403                    = serverassist._403;
 const _404                    = serverassist._404;
 const _500                    = serverassist._500;
 
-const appName                 = 's3';
+const myAppName               = 's3';
 const projectId               = 'sa';
-const appId                   = `${projectId}_${appName}`;
-const mount                   = `*/api/v1/${appName}/`;
+const appId                   = `${projectId}_${myAppName}`;
+const mount                   = `*/api/v1/${myAppName}/`;
 
 const appRecord = {
   projectId,
   mount,
   appId,
-  route               : `*/api/v:version/${appName}/`,
+  route               : `*/api/v:version/${myAppName}/`,
   isAdminApp          : false,
   useHttp             : true,
   useHttps            : true,
@@ -48,18 +50,21 @@ lib.addRoutes = function(addRoute, onStart, db /*, addRawRoute, callback */) {
   const addRawRoute   = args.shift();
   var   r;
 
+  var   access        = {};
+
   var s3 = new AWS.S3();
 
   //-------------------------------------------------------------------------------------------------
   /**
-   *  Handles xyz
+   *  Handles s3get
    *
-   *    /xyz
+   *    /s3get
    *
    */
   const s3get = function(req, res, params_, splats, query) {
     var   result      = {};
     const args_       = sg.extend(req.bodyJson || {}, params_ || {}, query || {});
+    const projectId   = argvGet(args_, 'project-id,projectId');
     const args        = _.omit(args_, 'projectId,version'.split(','));
 
     const start       = _.now();
@@ -70,6 +75,16 @@ lib.addRoutes = function(addRoute, onStart, db /*, addRawRoute, callback */) {
     if (!Bucket)      { return _400(req, res, 'Must provide Bucket'); }
     if (!Key)         { return _400(req, res, 'Must provide Key'); }
 
+    const allowRules  = deref(access, [projectId+'_'+myAppName, 'allow']) || [];
+    const allow       = sg.reduce(allowRules, null, (m, rule) => {
+      if (Bucket.match(rule.bucket) && Key.match(rule.key)) { return true; }
+      return m;
+    }) || false;
+
+console.log({allow});
+
+    if (!allow)       { return _403(req, res, `Access denied to ${Bucket}/${Key}`); }
+
     return sg.__run2({}, [function(result, next, last, abort) {
 
       const params = sg.reduce(args, {Bucket, Key}, (m, value, key) => {
@@ -77,14 +92,37 @@ lib.addRoutes = function(addRoute, onStart, db /*, addRawRoute, callback */) {
       });
 
       return s3.getObject(params, (err, data) => {
-        //console.log(err, _.keys(data), _.omit(data, 'Body'), data.Body.length);
+        if (err) {
+          console.log(_.pick(err, 'message,code,statusCode'.split(',')));
+          if (err.statusCode === 404)   { return _404(req, res, err.code || 'Not Found'); }
+          return _400(req, res, err);
+        }
 
+        //console.log(err, _.keys(data), _.omit(data, 'Body'), data.Body.length, typeof data.Body);
+
+        // If its JSON, return as such
         if (data.ContentType === 'application/json') {
           _.extend(result, sg.safeJSONParse(data.Body) || data.Body);
           return next();
         }
 
-        return next();
+        /* otherwise -- return according to the metadata that S3 has */
+        if (sg.isnt(delay)) {
+          return doit();
+        }
+
+        /* othewise */
+        const elapsed = _.now() - start;
+        return sg.setTimeout(delay - elapsed, doit);
+
+        function doit() {
+          res.writeHead(200, {
+            'Content-Type'    : data.ContentType,
+            'Content-Length'  : data.ContentLength
+          });
+          res.end(data.Body);
+        }
+
       });
 
     }], function last(err, result) {
@@ -118,14 +156,22 @@ lib.addRoutes = function(addRoute, onStart, db /*, addRawRoute, callback */) {
 
   }, function(next) {
     return configuration({}, {}, (err, r_) => {
-      if (err) { return sg.die(err, callback, `sa_${appName}.addRoutes.configuration`); }
+      if (err) { return sg.die(err, callback, `sa_${myAppName}.addRoutes.configuration`); }
 
       r = r_;
 
-      _.each(r.result.app_prj, (app_prj, app_prjName) => {
-        if (app_prj.app.appId !== appId) { return; }    /* this is not my app */
-        //console.log(`my app: ${app_prjName}`, sg.inspect(app_prj));
+      // Determine the access rules to buckets and keys
+      _.each(r.db.appprjRecords, appprjRecord => {
+        if (appprjRecord.appId !== myAppName) { return; }
+        const allow = _.map(appprjRecord.allow, rule => {
+          return {bucket:safeNewRegExp('^'+rule.bucket+'$'), key:safeNewRegExp('^'+rule.key)};
+        });
+        access[appprjRecord.appProjectId] = {allow};
       });
+
+      //_.each(r.result.app_prj, (app_prj, app_prjName) => {
+      //  if (app_prj.app.appId !== appId) { return; }    /* this is not my app */
+      //});
 
       return next();
     });
@@ -144,8 +190,10 @@ lib.addRoutes = function(addRoute, onStart, db /*, addRawRoute, callback */) {
       const [projectId, appName]  = app_prjName.split('_');
       const myMount               = deref(app_prj, [myStack, myColor, 'mount']) || '';
 
-      addRoute(`/:projectId(${projectId})/api/v:version/${appName}`, `/get`,       s3get, app_prjName);
-      addRoute(`/:projectId(${projectId})/api/v:version/${appName}`, `/get/*`,     s3get, app_prjName);
+      addRoute(`/:projectId(${projectId})/api/v:version/${appName}`, `/get`,                  s3get, app_prjName, true);
+      //addRoute(`/:projectId(${projectId})/api/v:version/${appName}`, `/get/:Bucket/:Key`,     s3get, app_prjName, true);      // Does not work right, yet
+      //addRoute(`/:projectId(${projectId})/api/v:version/${appName}`, `/get/:Bucket/:Key/*`,   s3get, app_prjName, true);      // Does not work right, yet
+      addRoute(`/:projectId(${projectId})/api/v:version/${appName}`, `/get/*`,                s3get, app_prjName, true);
 
       // Add startup notification handler for S3
       onStart.push(function(port, myIp) {
@@ -172,5 +220,14 @@ lib.addRoutes = function(addRoute, onStart, db /*, addRawRoute, callback */) {
 _.each(lib, (value, key) => {
   exports[key] = value;
 });
+
+function mySafeNewRegExp(str) {
+  try {
+    return new RegExp(str);
+  } catch(e) {
+  }
+
+  return /*undefined*/;
+}
 
 
